2fd01ddd9d99330cc7eaedf23efec60629d00550
Littlesqx <littlesqx@gmail.com>
2019-03-07 14:30:50 +0800

    [go] add go-rest-api/ch03

---
+++ go/go-rest-api/ch03/config/config.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"strings"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"github.com/lexkong/log"
+)
+
+type Config struct {
+	Name string
+}
+
+func (c *Config) initConfig() error {
+	if c.Name != "" {
+		viper.SetConfigFile(c.Name)
+	} else {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()			// 读取匹配的环境变量
+	viper.SetEnvPrefix("APISERVER")	// 读取环境变量的前缀为 APISERVER
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	return viper.ReadInConfig() // nil|error
+}
+
+func (c *Config) watchConfig() {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Infof("Config file changed: %s", e.Name)
+	})
+}
+
+func (c *Config) initLog() {
+	passLagerCfg := log.PassLagerCfg {
+		Writers: viper.GetString("log.writers"),
+		LoggerLevel: viper.GetString("log.logger_level"),
+		LoggerFile: viper.GetString("log.logger_file"),
+		LogFormatText: viper.GetBool("log.log_format_text"),
+		RollingPolicy: viper.GetString("log.rollingPolicy"),
+		LogRotateDate: viper.GetInt("log.log_rotate_date"),
+		LogRotateSize: viper.GetInt("log.log_rotate_size"),
+		LogBackupCount: viper.GetInt("log.log_backup_count"),
+	}
+	log.InitWithConfig(&passLagerCfg)
+}
+
+func Init(config string) error {
+	c := Config {
+		Name: config,
+	}
+	if err := c.initConfig(); err != nil {
+		return err
+	}
+
+	c.initLog()
+	
+	c.watchConfig()
+
+	return nil
+}
+
