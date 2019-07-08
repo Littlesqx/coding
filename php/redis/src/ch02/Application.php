9245c4c03b36513c56b256bf5875e706bb4ef24d
Littlesqx <Littlesqx@gmail.com>
2019-07-08 16:53:58 +0800

    [php] Update predis ch02

--- php/redis/src/ch02/Application.php
+++ php/redis/src/ch02/Application.php
@@ -9,16 +9,50 @@ class Application extends AbstractApplication
 
     public function run()
     {
-        var_dump($this->redis->set('lock:codehole', true, 'ex', 5, 'nx'));
-        var_dump($this->redis->set('lock:codehole', true, 'ex', 5, 'nx'));
-        var_dump($this->redis->del(['lock:codehole']));
+//        var_dump($this->redis->set('lock:codehole', true, 'ex', 5, 'nx'));
+//        var_dump($this->redis->set('lock:codehole', true, 'ex', 5, 'nx'));
+//        var_dump($this->redis->del(['lock:codehole']));
 
-        $tag = uniqid();
-        if ($this->redis->set('lock:codehole', $tag, 'ex', 5, 'nx')) {
+        $tag = 'lock:codehole:tag';
+
+        if ($this->redis->set('lock:codehole', $tag, 'ex', 10, 'nx')) {
             // do something
-            // # !! not atomicity
-            $tag == $this->redis->get('lock:codehole') && $this->redis->del('lock:codehole');
+            echo getmypid() . ": I have the lock. \n";
+            $i = 0;
+            while ($i++ < 3) {
+                echo "$i\n";
+                sleep(1);
+            }
+
+            var_dump($this->delIfEqualsAtomicity('lock:codehole', $tag));
+
+            return;
         }
-        var_dump($this->redis->get('lock:codehole'));
+
+        echo getmypid() . ": I can not have the lock. \n";;
+
+    }
+
+    public function delIfEquals($key, $tag)
+    {
+        // # !! not atomicity
+        $tag == $this->redis->get($key) && $this->redis->del($key);
+    }
+
+    public function delIfEqualsAtomicity($key, $tag)
+    {
+        return $this->redis->eval(
+            '
+            -- delifequals
+            if redis.call("get",KEYS[1]) == ARGV[1] then
+                return redis.call("del",KEYS[1])
+            else
+                return 0
+            end
+            ',
+            1,
+            $key,
+            $tag
+        );
     }
 }
